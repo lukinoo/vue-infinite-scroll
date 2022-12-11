b48853403eb0b810007c0378098a1edc1a973423
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-11 16:57:52 +0400

    ✨NEW: adding vue-styled-components

--- src/view/InfiniteScroll/InfiniteScroll.vue
+++ src/view/InfiniteScroll/InfiniteScroll.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-const count = ref(0);
+import MovieVue from "../Movie/Movie.vue";
 </script>
 <template>
-  <h1>{{ count }}</h1>
-  <button @click="count += 1">count</button>
-  <button @click="count -= 1">count</button>
+  <MovieVue />
 </template>
 <style scoped lang="less">
 @primaryBlack: #000;
