8f15556ed1a1512888552fe85beeec0e1f48f5bd
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-11 19:10:14 +0400

    ✨NEW: adding costum usefetch hook for data fetching

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import InfiniteScrollVue from "./components/InfiniteScroll/InfiniteScroll.vue";
 import Loading from "./components/Loading/Loading.vue";
+import { TMovies } from "./types";
+import { useFetch } from "./hooks/useFetch";
+import { onBeforeMount } from "vue";
+
+const apikey = `https://omdbapi.com/?s=spider+man&page=1&apikey=${
+  import.meta.env.VITE_API_KEY as string
+}`;
+
+// onBeforeMount(async () => {
+//   const { data, isLoading } = await useFetch<TMovies>(x);
+// });
 </script>
 
 <template>
